c9b07dcfd48dc1267e3d5419e419b744b1efd37f
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-02-23 06:57:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "26847865"))
 API_HASH = environ.get("API_HASH", "0ef9fdd3e5f1ed49d4eb918a07b8e5d6")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7350676839:AAEM1YYpXT4keuhWoc-WYRSuAHfwoHkqouw")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://envs.sh/Fmq.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5597521952').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Test44343_robot") # without @
 PORT = environ.get("PORT", "8080")
